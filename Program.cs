7816e21669a1870ea824b4ab5447bccb0c38df2b
Addison Roy <74374312+mama-sucker@users.noreply.github.com>
2023-10-05 14:00:26 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿# Module 4.1 
+
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Perfect Number finder\n");
+        Console.Write("Enter a number (1 - 10000): ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number) && number >= 1 && number <= 10000)
+        {
+            int sum = 0;
+
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            if (sum == number)
+            {
+                Console.WriteLine($"{number} perfect number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} not a perfect number.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Enter a number, not a letter!");
+            Console.WriteLine("Try again");
+        }
+    }
+}
+
